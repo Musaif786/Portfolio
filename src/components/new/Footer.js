b5952021b5dae8059e36fc45aa70a183e1ab874b
Musaif <mdmmusaif.mm@gmail.com>
2023-04-25 01:20:17 +0530

    "initial changes added "

--- src/components/new/Footer.js
+++ src/components/new/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
 </a></li>
           <li><a href="https://github.com/Musaif786">Github <i class="fa fa-github" aria-hidden="true"></i>
 </a></li>
-          <li><a href="https://www.instagram.com/md_programmer/">Instagram <i class="fa fa-instagram" aria-hidden="true"></i>
+          <li><a href="https://www.instagram.com">Instagram <i class="fa fa-instagram" aria-hidden="true"></i>
 </a></li>
           <li><a href="https://wa.me/+917995587687 ">WhatsApp <i class="fa fa-whatsapp" aria-hidden="true"></i>
 </a></li>
--- src/components/new/One.js
+++ src/components/new/One.js
@@ -4,32 +4,28 @@ import Contact from "./Contact";
 import Register from "./Register";
 import Footer from "./Footer";
 
-
 function One() {
+  useEffect(() => {
+    const imgFull = () => {
+      let img = document.querySelector(".about-img");
+      img.addEventListener("click", () => {
+        //  img.style.height = "500px";
+        img.classList.toggle("aboutImg");
+      });
+    };
+    imgFull();
+  }, []);
 
-  useEffect(()=>{
-   const imgFull =()=>{
-     let img = document.querySelector(".about-img");
-     img.addEventListener("click",()=>{
-      //  img.style.height = "500px";
-      img.classList.toggle("aboutImg");
-     });
-   }
-   imgFull();
-  },[])
-  
-  
-  
   //const width = window.innerWidth;
   return (
     <>
       <main>
         <div className="wrapper">
-          <div className="h1-box" data-aos="zoom-in-down"
->
+          <div className="h1-box" data-aos="zoom-in-down">
             <h1>
-              Hi, I am <span>Mohammed Musaif</span> Full stack React developer & splunk admin/ Developer in solugenix india
-              </h1>
+              Hi, I am <span>Mohammed Musaif</span> Full stack React developer &
+              splunk admin/ Developer in solugenix india
+            </h1>
           </div>
           <div className="img-box">
             <img
@@ -40,115 +36,59 @@ function One() {
         </div>
 
         <div id="about" className="about">
-        
-          <div className="about-h1" data-aos="zoom-in-down"
->
+          <div className="about-h1" data-aos="zoom-in-down">
             <h1>About</h1>
           </div>
-          <div className="about-container" data-aos="zoom-in-down"
->
+          <div className="about-container" data-aos="zoom-in-down">
             <div className="about-img">
-              <img
-                src="./images/musaif.jpg"
-              alt="auto/img"
-              
-              />
+              <img src="./images/musaif.jpg" alt="auto/img" />
             </div>
             <h3 data-aos="zoom-in-down">Mohammed Musaif</h3>
-            <p data-aos="zoom-in-down"
->
-             my name is Md Musaif. i'm from Hyderabad, telangana. i did my graduation in b.tech from CMR engineering college.
-             <button ><a href="https://musaifresume.000webhostapp.com/">More</a></button>
+            <p data-aos="zoom-in-down">
+              my name is Md Musaif. i'm from Hyderabad, telangana. i did my
+              graduation in b.tech from CMR engineering college.
+              <button>
+                <a href="https://musaifresume.000webhostapp.com/">More</a>
+              </button>
             </p>
           </div>
         </div>
 
         <div id="skills" className="skills">
-          <div className="about-h1" data-aos="zoom-in-down"
->
+          <div className="about-h1" data-aos="zoom-in-down">
             <h1>Skills</h1>
           </div>
           <div className=" skills-container">
-            <h3 data-aos="zoom-in-down" > professional skills </h3>
+            <h3 data-aos="zoom-in-down"> professional skills </h3>
             <div className="progress-box">
-              <progress
-                data-aos="zoom-in-down"
-
-                value="90"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="90" max="100"></progress>
               <p>HTML</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="90"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="90" max="100"></progress>
               <p>CSS</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="80"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="80" max="100"></progress>
               <p>JavaScript</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="80"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="80" max="100"></progress>
               <p>React-js</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="80"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="80" max="100"></progress>
               <p>Bootstrap</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="80"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="80" max="100"></progress>
               <p>Core Java</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="60"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="60" max="100"></progress>
               <p>Python</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="60"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="60" max="100"></progress>
               <p>Firebase</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="40"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="40" max="100"></progress>
               <p>basic Reactive-Native</p>
               <hr />
-              <progress
-                data-aos="zoom-in-down"
-
-                value="50"
-                max="100"
-              ></progress>
+              <progress data-aos="zoom-in-down" value="50" max="100"></progress>
               <p>Mysqli CRUD</p>
               <hr />
             </div>
@@ -156,90 +96,105 @@ function One() {
         </div>
 
         <div id="tools" className="Tools">
-          <div data-aos="zoom-in-down"
- className="tools-h1">
+          <div data-aos="zoom-in-down" className="tools-h1">
             <h1>Tools</h1>
           </div>
           <div className="tools-container">
             <ol>
-              <li data-aos="zoom-in-down"
->VSCode</li>
-              <li data-aos="zoom-in-down"
->Eclipse</li>
-              <li data-aos="zoom-in-down"
->Github</li>
+              <li data-aos="zoom-in-down">VSCode</li>
+              <li data-aos="zoom-in-down">Eclipse</li>
+              <li data-aos="zoom-in-down">Github</li>
             </ol>
           </div>
         </div>
-        
 
         <div id="project" className="tools projects">
-          <div data-aos="zoom-in-down"
- className="tools-h1 project-h1">
+          <div data-aos="zoom-in-down" className="tools-h1 project-h1">
             {" "}
             <h1>Projects</h1>
           </div>
           <div className="projects">
             <div className="projects-imgs">
-            <a href="https://zfwork.netlify.app/"> <img data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000"
- 
-                src="./images/Screenshot (39).png"
-                alt="auto/img"
-              /></a>
+              <p data-aos="zoom-in-down">
+                Below are the projects which are created and developed by Musaif.
+                by click on the photo you will redirect to main website. aliquam
+                possimus dolorum recusandae laudantium assumenda incidunt veritatis sint vitae.
+              </p>
+              <a href="https://zfwork.netlify.app/">
+                {" "}
+                <img
+                  data-aos="flip-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="2000"
+                  src="./images/Screenshot (39).png"
+                  alt="auto/img"
+                />
+              </a>
 
-<a href="https://musaifchat.netlify.app/"> <img data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000"
- 
-                src="./images/Screenshot (35).png"
-                alt="auto/img"
-              /></a>
+              <a href="https://musaifchat.netlify.app/">
+                {" "}
+                <img
+                  data-aos="flip-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="2000"
+                  src="./images/Screenshot (35).png"
+                  alt="auto/img"
+                />
+              </a>
 
+              <a href="http://musaifofficial.000webhostapp.com/">
+                {" "}
+                <img
+                  data-aos="zoom-in-down"
+                  src="./images/Screenshot (36).png"
+                  alt="auto/img"
+                />
+              </a>
 
-<a href="http://musaifofficial.000webhostapp.com/"> <img data-aos="zoom-in-down"
- 
-                src="./images/Screenshot (36).png"
-                alt="auto/img"
-              /></a>
+              <a href="https://musaif-database.netlify.app/">
+                {" "}
+                <img
+                  data-aos="flip-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="2000"
+                  src="./images/Screenshot (38).png"
+                  alt="auto/img"
+                />
+              </a>
 
-<a href="https://musaif-database.netlify.app/"> <img data-aos="flip-left"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000"
- 
-                src="./images/Screenshot (38).png"
-                alt="auto/img"
-              /></a>
-
-<a href="http://musaifofficial.000webhostapp.com/"> <img data-aos="flip-right"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000"
- 
-                src="./images/Screenshot (37).png"
-                alt="auto/img"
-              /></a>
-              <a href="https://musaifresume.000webhostapp.com/"> <img data-aos="zoom-in-down"
- 
-                src="./images/Screenshot (40).png"
-                alt="auto/img"
-              /></a>
-
-
+              <a href="http://musaifofficial.000webhostapp.com/">
+                {" "}
+                <img
+                  data-aos="flip-right"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="2000"
+                  src="./images/Screenshot (37).png"
+                  alt="auto/img"
+                />
+              </a>
+              <a href="https://musaifresume.000webhostapp.com/">
+                {" "}
+                <img
+                  data-aos="zoom-in-down"
+                  src="./images/Screenshot (40).png"
+                  alt="auto/img"
+                />
+              </a>
             </div>
           </div>
         </div>
-    <div id="contact"  data-aos="flip-right"
-     data-aos-easing="ease-out-cubic"
-     data-aos-duration="2000">
-      <Register/>
-    </div>
+        <div
+          id="contact"
+          data-aos="flip-right"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="2000"
+        >
+          <Register />
+        </div>
 
-    <div id="footer" className="footer">
-  <Footer/>
-    </div>
-
-
+        <div id="footer" className="footer">
+          <Footer />
+        </div>
 
         {/* <div className="tools contact">
         <div className="tools-h1"><h1>Contact</h1> </div>
