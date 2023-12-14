246183bb7026b29f2d51e4bc8dffd9a57d9dc232
Musaif786 <mdmusaif.mm@gmail.com>
2023-12-14 12:55:27 +0530

    updated footer

--- src/components/new/Footer.js
+++ src/components/new/Footer.js
@@ -5,35 +5,38 @@ import "../newCss/Footer.css";
 function Footer() {
   // const current_date = new Date()
   // const current_new = new Date()
-  const fixeddate = moment('2022-02-02','YYYY-MM-DD') 
+  const fixeddate = moment('2022-02-02', 'YYYY-MM-DD')
   const currentdate = moment()
-  const differentDate = currentdate.diff(fixeddate,'days')
-  const style ={
+  const differentDate = currentdate.diff(fixeddate, 'days')
+  const formattedDate = currentdate.format('DD-MMMM-YYYY');
+  const fullMonthName = moment(formattedDate, 'DD-MMMM-YYYY').format('DD-MMMM-YYYY');
+
+  const style = {
     letterSpacing: '1px',
     color: 'black',
     fontSize: '16px',
     textDecoration: 'none',
-    cursor:"default",
+    cursor: "default",
 
   }
   return (
     <>
-        <footer>
-         <ul>
+      <footer>
+        <ul>
           <li><a href="https://www.facebook.com/profile.php?id=100006983837465">Facebook <i class="fa fa-facebook" aria-hidden="true"></i>
 
-</a></li>
+          </a></li>
           <li><a href="https://github.com/Musaif786">Github <i class="fa fa-github" aria-hidden="true"></i>
-</a></li>
+          </a></li>
           <li><a href="https://www.instagram.com">Instagram <i class="fa fa-instagram" aria-hidden="true"></i>
-</a></li>
+          </a></li>
           <li><a href="https://wa.me/+917995587687 ">WhatsApp <i class="fa fa-whatsapp" aria-hidden="true"></i>
-</a></li>
-         </ul>
-        </footer>
-         <div className='footer-div'>
-            <a style={style} href='https://tinyurl.com/yr4q7d95/speak?speak=Hello world!'> Musaif</a>	Created &copy;{differentDate} Days Ago, Now &copy;{currentdate.format('YYYY-MM-DD')}
-         </div>
+          </a></li>
+        </ul>
+      </footer>
+      <div className='footer-div'>
+        <a style={style} href='https://tinyurl.com/yr4q7d95/speak?speak=Hello world!'> "Musaif</a>, the creator of the portfolio, established it &copy;{differentDate} Days Ago, As of today's date, {fullMonthName}"
+      </div>
     </>
   )
 }
